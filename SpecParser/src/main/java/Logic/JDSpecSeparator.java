cb2c838c7bac33c5adba002d621a1cf814d8c875
Nina Gonschorreck <nina.gonschorreck@bundesbank.de>
2015-04-14 14:00:12 +0200

    ready is Transformation, Outlier, ARIMA, Automdl, Estimate, Basic

--- SpecParser/src/main/java/Logic/JDSpecSeparator.java
+++ SpecParser/src/main/java/Logic/JDSpecSeparator.java
@@ -9,7 +9,14 @@ import ec.satoolkit.x11.SeasonalFilterOption;
 import ec.satoolkit.x11.X11Specification;
 import ec.satoolkit.x13.X13Specification;
 import ec.tss.Ts;
+import ec.tstoolkit.Parameter;
+import ec.tstoolkit.modelling.DefaultTransformationType;
+import ec.tstoolkit.modelling.arima.x13.ArimaSpec;
+import ec.tstoolkit.modelling.arima.x13.AutoModelSpec;
+import ec.tstoolkit.modelling.arima.x13.OutlierSpec;
 import ec.tstoolkit.modelling.arima.x13.RegArimaSpecification;
+import ec.tstoolkit.modelling.arima.x13.SingleOutlierSpec;
+import ec.tstoolkit.modelling.arima.x13.TransformSpec;
 import ec.tstoolkit.timeseries.TsPeriodSelector;
 import eu.nbdemetra.specParser.Miscellaneous.SpecificationPart;
 import java.util.ArrayList;
@@ -32,17 +39,397 @@ public class JDSpecSeparator {
         this.ts = ts;
         this.spec = x13;
 
-        buildX11Spec();
-        buildForecastSpec();
-        buildSERIES();
-//        RegArimaSpecification reg = x13.getRegArimaSpecification();
+        generateTs();
+
+        generateBasicSpec();
+        generateEstimateSpec();
+        generateTransformSpec();
+
+        if (spec.getRegArimaSpecification().getAutoModel().isEnabled()) {
+            generateAutomdl();
+        } else {
+            generateArima();
+        }
+        generateOutlier();
+        generateX11Spec();
+    }
+
+    private void generateArima() {
+
+        ArimaSpec reg = spec.getRegArimaSpecification().getArima();
+        TreeMap<String, String> arima;
+
+        if (result.containsKey(SpecificationPart.ARIMA)) {
+            arima = result.get(SpecificationPart.ARIMA);
+        } else {
+            arima = new TreeMap<>();
+        }
+
+//        1)model
+        String model = "( " + reg.getP() + " " + reg.getD() + " " + reg.getQ() + " )( " + reg.getBP() + " " + reg.getBD() + " " + reg.getBQ() + " )";
+        arima.put("model", model);
+
+//        2)AR
+        StringBuilder ar = null;
+        if (reg.getP() > 0) {
+            ar = new StringBuilder("( ");
+            Parameter p;
+            for (int i = 0; i < reg.getP(); i++) {
+                p = reg.getPhi()[i];
+                ar.append(p.getValue() * -1);
+                switch (p.getType()) {
+                    case Fixed:
+                        ar.append("f");
+                        break;
+                    default:
+                        break;
+                }
+                if (i < (reg.getP() - 1) || reg.getBP() > 0) {
+                    ar.append(" , ");
+                }
+            }
+        }
+        if (reg.getBP() > 0) {
+            if (ar == null) {
+                ar = new StringBuilder("( ");
+            }
+            Parameter p;
+            for (int i = 0; i < reg.getBP(); i++) {
+                p = reg.getBPhi()[i];
+                ar.append(p.getValue() * -1);
+                switch (p.getType()) {
+                    case Fixed:
+                        ar.append("f");
+                        break;
+                    default:
+                        break;
+                }
+                if (i < (reg.getBP() - 1)) {
+                    ar.append(" , ");
+                }
+            }
+        }
+        if (ar != null) {
+            ar.append(" )");
+            arima.put("ar", ar.toString());
+        }
+
+//        3)MA
+        StringBuilder ma = null;
+        if (reg.getQ() > 0) {
+            ma = new StringBuilder("( ");
+            Parameter p;
+            for (int i = 0; i < reg.getQ(); i++) {
+                p = reg.getTheta()[i];
+                ma.append(p.getValue() * -1);
+                switch (p.getType()) {
+                    case Fixed:
+                        ma.append("f");
+                        break;
+                    default:
+                        break;
+                }
+                if (i < (reg.getQ() - 1) || reg.getBQ() > 0) {
+                    ma.append(" , ");
+                }
+            }
+        }
+        if (reg.getBQ() > 0) {
+            if (ma == null) {
+                ma = new StringBuilder("( ");
+            }
+            Parameter p;
+            for (int i = 0; i < reg.getBQ(); i++) {
+                p = reg.getBTheta()[i];
+                ma.append(p.getValue() * -1);
+                switch (p.getType()) {
+                    case Fixed:
+                        ma.append("f");
+                        break;
+                    default:
+                        break;
+                }
+                if (i < (reg.getBQ() - 1)) {
+                    ma.append(" , ");
+                }
+            }
+        }
+        if (ma != null) {
+            ma.append(" )");
+            arima.put("ma", ma.toString());
+        }
+
+//        4)Mean
+        if (reg.isMean()) {
+            errors.add(SpecificationPart.ARIMA + ": Mean is not supported in WinX12");
+        }
+
+        result.put(SpecificationPart.ARIMA, arima);
+    }
+
+    private void generateAutomdl() {
+
+        AutoModelSpec reg = spec.getRegArimaSpecification().getAutoModel();
+        TreeMap<String, String> automdl;
+
+        if (result.containsKey(SpecificationPart.AUTOMDL)) {
+            automdl = result.get(SpecificationPart.AUTOMDL);
+        } else {
+            automdl = new TreeMap<>();
+        }
+
+//        1)accept default
+        String acceptdef;
+        if (reg.isAcceptDefault()) {
+            acceptdef = "yes";
+        } else {
+            acceptdef = "no";
+        }
+        automdl.put("acceptdefault", acceptdef);
+
+//        2)Balanced
+        String balanced;
+        if (reg.isBalanced()) {
+            balanced = "yes";
+        } else {
+            balanced = "no";
+        }
+        automdl.put("balanced", balanced);
+
+//        3)cancelation limit
+        errors.add(SpecificationPart.AUTOMDL + ": No Translation for Cancelation limit");
+
+        //4)checkmu
+        String checkmu;
+        if (reg.isCheckMu()) {
+            checkmu = "yes";
+        } else {
+            checkmu = "no";
+        }
+        automdl.put("checkmu", checkmu);
+
+//        5)ljungboxlimit
+        automdl.put("ljungboxlimit", reg.getLjungBoxLimit() + "");
+
+//        6)mixed
+        String mixed;
+        if (reg.isMixed()) {
+            mixed = "yes";
+        } else {
+            mixed = "no";
+        }
+        automdl.put("mixed", mixed);
+
+//        7)reducecv
+        automdl.put("reducecv", reg.getPercentReductionCV() + "");
+
+//        8)unit root limit
+        automdl.put("urfinal", reg.getUnitRootLimit() + "");
+
+//        9)final + initial unit root limit
+        errors.add(SpecificationPart.AUTOMDL + ": There is no translation for final and initial unit root limit");
+
+//        10)hrinitial
+        String hr;
+        if (reg.isHannanRissannen()) {
+            hr = "yes";
+        } else {
+            hr = "no";
+        }
+        automdl.put("hrinitial", hr);
+
+        result.put(SpecificationPart.AUTOMDL, automdl);
+    }
+
+    private void generateBasicSpec() {
+
+        //1) span
+        RegArimaSpecification reg = spec.getRegArimaSpecification();
+        TreeMap<String, String> series;
+
+        if (result.containsKey(SpecificationPart.SERIES)) {
+            series = result.get(SpecificationPart.SERIES);
+        } else {
+            series = new TreeMap<>();
+        }
+
+        String span = separateSpan(reg.getBasic().getSpan(), "SERIES");
+        if (span != null) {
+            series.put("span", span);
+        }
+        result.put(SpecificationPart.SERIES, series);
+    }
+
+    private void generateEstimateSpec() {
+
+        RegArimaSpecification reg = spec.getRegArimaSpecification();
+
+//        1)modelspan
+        TreeMap<String, String> series;
+        if (result.containsKey(SpecificationPart.SERIES)) {
+            series = result.get(SpecificationPart.SERIES);
+        } else {
+            series = new TreeMap<>();
+        }
+
+        String span = separateSpan(reg.getEstimate().getSpan(), "ESTIMATE");
+        if (span != null) {
+            series.put("modelspan", span);
+        }
+
+        result.put(SpecificationPart.SERIES, series);
+
+//        2)tol
+        TreeMap<String, String> estimate;
+        if (result.containsKey(SpecificationPart.ESTIMATE)) {
+            estimate = result.get(SpecificationPart.ESTIMATE);
+        } else {
+            estimate = new TreeMap<>();
+        }
+        estimate.put("tol", reg.getEstimate().getTol() + "");
+
+        result.put(SpecificationPart.ESTIMATE, estimate);
+    }
+
+    private void generateOutlier() {
+
+        OutlierSpec reg = spec.getRegArimaSpecification().getOutliers();
+        TreeMap<String, String> outlier;
+        if (result.containsKey(SpecificationPart.OUTLIER)) {
+            outlier = result.get(SpecificationPart.OUTLIER);
+        } else {
+            outlier = new TreeMap<>();
+        }
+
+//        1)span
+        String span = separateSpan(reg.getSpan(), "OUTLIER");
+        if (span != null) {
+            outlier.put("span", span);
+        }
+
+//        2)LSRun
+        outlier.put("lsrun", reg.getLSRun() + "");
+
+        if (reg.isUsed()) {
+
+//            1)critical value
+            outlier.put("critical", reg.getDefaultCriticalValue() + "");
+
+//            2)TC rate
+            outlier.put("tcrate", reg.getMonthlyTCRate() + "");
+
+//            3)Method
+            outlier.put("method", reg.getMethod().toString());
+
+//            4)types
+            StringBuilder types;
+            if (reg.getTypes() == null) {
+                types = new StringBuilder("none");
+            } else {
+                types = new StringBuilder("( ");
+                for (SingleOutlierSpec s : reg.getTypes()) {
+                    switch (s.getType()) {
+                        case AO:
+                        case LS:
+                        case TC: types.append(s.getType()).append(" "); break;
+                        default: errors.add(SpecificationPart.OUTLIER+": No Translation for "+s.getType().name()); break;
+                    }
+                }
+                types.append(")");
+            }
+            outlier.put("types", types.toString());
+        }
+
+        result.put(SpecificationPart.OUTLIER, outlier);
+    }
+
+    private void generateTransformSpec() {
+
+        TransformSpec t = spec.getRegArimaSpecification().getTransform();
+        TreeMap<String, String> transform;
+        if (result.containsKey(SpecificationPart.TRANSFORM)) {
+            transform = result.get(SpecificationPart.TRANSFORM);
+        } else {
+            transform = new TreeMap<>();
+        }
+
+//        1)function
+        transform.put("function", t.getFunction().name());
+
+//        2)aic diff
+        if (t.getFunction().equals(DefaultTransformationType.Auto)) {
+            transform.put("aicdiff", t.getAICDiff() + "");
+        }
+
+//        3)adjust
+        if (t.getFunction().equals(DefaultTransformationType.Log)) {
+            String adjust;
+            switch (t.getAdjust()) {
+                case None:
+                    adjust = "none";
+                    break;
+                case LeapYear:
+                    adjust = "lpyear";
+                    break;
+                case LengthOfPeriod:
+                    if (ts.getTsData().getFrequency().intValue() == 12) {
+                        adjust = "lom";
+                    } else {
+                        adjust = "loq";
+                    }
+                    break;
+                default:
+                    errors.add(SpecificationPart.TRANSFORM + ": No translation for " + t.getAdjust());
+                    adjust = null;
+                    break;
+            }
+
+            if (adjust != null) {
+                transform.put("adjust", adjust);
+            }
+        }
+
+        result.put(SpecificationPart.TRANSFORM, transform);
+    }
+
+    private void generateTs() {
+
+        TreeMap<String, String> series;
+        if (result.containsKey(SpecificationPart.SERIES)) {
+            series = result.get(SpecificationPart.SERIES);
+        } else {
+            series = new TreeMap<>();
+        }
+
+        //1) start
+        StringBuilder start = new StringBuilder(ts.getTsData().getStart().getYear() + ".");
+        start.append(ts.getTsData().getStart().getPosition() + 1);
+        series.put("start", start.toString());
+
+//        2) period
+        series.put("period", ts.getTsData().getFrequency().intValue() + "");
+
+//        3) data
+        series.put("data", "(" + ts.getTsData().getValues().toString() + ")");
+
+//        4) title
+        series.put("title", ts.getRawName());
+
+        result.put(SpecificationPart.SERIES, series);
 
     }
 
-    private void buildX11Spec() {
+    private void generateX11Spec() {
 
+//        A)X11
         X11Specification x11 = spec.getX11Specification();
-        TreeMap<String, String> x11Result = new TreeMap();
+        TreeMap<String, String> x11Result;
+
+        if (result.containsKey(SpecificationPart.X11)) {
+            x11Result = result.get(SpecificationPart.X11);
+        } else {
+            x11Result = new TreeMap<>();
+        }
 
         //1) Mode
         String mode;
@@ -89,64 +476,22 @@ public class JDSpecSeparator {
         x11Result.put("sigmalim", "(" + x11.getLowerSigma() + " , " + x11.getUpperSigma() + " )");
 
         result.put(SpecificationPart.X11, x11Result);
-    }
 
-    private void buildSERIES() {
-
-        TreeMap<String, String> series = new TreeMap();
-        RegArimaSpecification reg = spec.getRegArimaSpecification();
-
-        //1) start
-        StringBuilder start = new StringBuilder(ts.getTsData().getStart().getYear() + ".");
-        start.append(ts.getTsData().getStart().getPosition() + 1);
-        series.put("start", start.toString());
-
-        //2) span
-        String span = separateSpan(reg.getBasic().getSpan(), "SERIES");
-        if (span != null) {
-            series.put("span", span);
+//        B) FORECAST
+        TreeMap<String, String> forecast;
+        if (result.containsKey(SpecificationPart.FORECAST)) {
+            forecast = result.get(SpecificationPart.FORECAST);
+        } else {
+            forecast = new TreeMap<>();
         }
-
-//        3) modelspan
-        span = separateSpan(reg.getEstimate().getSpan(), "ESTIMATE");
-        if (span != null) {
-            series.put("modelspan", span);
-        }
-        
-//        4) period
-        series.put("period", ts.getTsData().getFrequency().intValue()+"");
-        
-//        5) data
-        series.put("data","("+ ts.getTsData().getValues().toString()+")");
-
-        result.put(SpecificationPart.SERIES, series);
-
-    }
-
-    private void buildForecastSpec() {
-
-        X11Specification x11 = spec.getX11Specification();
-        TreeMap<String, String> forecast = new TreeMap();
-        //1) Maxlead
+//        1) maxlead/forecast horizon
         int maxlead = x11.getForecastHorizon();
         if (maxlead < 0) {
             maxlead = -1 * maxlead * ts.getTsData().getFrequency().intValue();
         }
         forecast.put("maxlead", maxlead + "");
+
         result.put(SpecificationPart.FORECAST, forecast);
-
-    }
-
-    private void buildRegArima() {
-
-        RegArimaSpecification reg = spec.getRegArimaSpecification();
-        if (reg.isUsingAutoModel()) {
-            //Automdl
-        } else {
-            //Arima
-        }
-
-        //ansonsten den rest aufrufen
     }
 
     public String getResult() {
@@ -154,7 +499,7 @@ public class JDSpecSeparator {
         StringBuilder text = new StringBuilder();
 
         for (SpecificationPart spec : result.keySet()) {
-            text.append(spec).append("{\n");
+            text.append(spec).append("\n{\n");
 
             TreeMap<String, String> content = result.get(spec);
 
@@ -162,7 +507,7 @@ public class JDSpecSeparator {
                 text.append("\t").append(argument).append(" = ").append(content.get(argument)).append("\n");
             }
 
-            text.append("}\n");
+            text.append("}\n\n");
         }
 
         return text.toString();
@@ -173,9 +518,9 @@ public class JDSpecSeparator {
     }
 
     public String[] getMessageList() {
-        return messages.toArray(new String[messages.size()]); 
+        return messages.toArray(new String[messages.size()]);
     }
-    
+
     private String separateSpan(TsPeriodSelector p, String specPart) {
 
         String result;
@@ -185,13 +530,13 @@ public class JDSpecSeparator {
                 result = null;
                 break;
             case From:
-                result = "(" + p.getD0().getYear() + "." + (p.getD0().getMonth()+1) + ", )";
+                result = "(" + p.getD0().getYear() + "." + (p.getD0().getMonth() + 1) + ", )";
                 break;
             case To:
-                result = "( ," + p.getD1().getYear() + "." + (p.getD1().getMonth()+1) + ")";
+                result = "( ," + p.getD1().getYear() + "." + (p.getD1().getMonth() + 1) + ")";
                 break;
             case Between:
-                result = "(" + p.getD0().getYear() + "." + (p.getD0().getMonth()+1) + " , " + p.getD1().getYear() + "." + p.getD1().getMonth() + ")";
+                result = "(" + p.getD0().getYear() + "." + (p.getD0().getMonth() + 1) + " , " + p.getD1().getYear() + "." + p.getD1().getMonth() + ")";
                 break;
             case Excluding:
             case First:
@@ -205,5 +550,5 @@ public class JDSpecSeparator {
         }
         return result;
     }
-    
+
 }
